b3ea9849a58de78eeedf6df91e81f19bb1fa32b6
SeulGiLee <ghre55@git.co.kr>
2017-06-25 20:06:49 +0900

    commit

--- OpenGDS_2017/src/main/java/com/git/opengds/geoserver/service/GeoserverDBServiceImpl.java
+++ OpenGDS_2017/src/main/java/com/git/opengds/geoserver/service/GeoserverDBServiceImpl.java
@@ -7,27 +7,35 @@ import com.git.gdsbuilder.FileRead.en.EnFileFormat;
 
 public class GeoserverDBServiceImpl {
 
-	public boolean selectEditLayerDuplicateCheck(EnFileFormat fileFormat, String fileName, String nativeLayerName, String currentLayerName){
+	public boolean selectEditLayerDuplicateCheck(EnFileFormat fileFormat, String fileName, String layerName){
 		boolean dupFlag = true;
 		Map<String, Object> parmasMap = new HashMap<String, Object>();
 		if(fileFormat == EnFileFormat.DXF){
 			parmasMap.put("tableName", "public.qa10_layer_metadata");
 			parmasMap.put("fileName", fileName);
-			parmasMap.put("nativeNameCol", "");
-			parmasMap.put("nativeNameVal", nativeLayerName);
-			parmasMap.put("currentNameCol", "");
-			parmasMap.put("currentNameVal", currentLayerName);
+			parmasMap.put("nativeNameCol", "layer_id");
+			parmasMap.put("currentNameCol", "current_layer_name");
+			parmasMap.put("layerName", layerName);
 		}
 		else if(fileFormat == EnFileFormat.NGI){
-			parmasMap.put("tableName", "");
+			parmasMap.put("tableName", "public.qa20_layer_metadata");
+			parmasMap.put("fileName", fileName);
+			parmasMap.put("nativeNameCol", "layer_name");
+			parmasMap.put("currentNameCol", "current_layer_name");
+			parmasMap.put("layerName", layerName);
 		}
 		else if(fileFormat == EnFileFormat.SHP){
-			parmasMap.put("tableName", "");
+			parmasMap.put("tableName", "public.qa10_layer_metadata");
+			parmasMap.put("fileName", fileName);
+			parmasMap.put("nativeNameCol", "layer_id");
+			parmasMap.put("currentNameCol", "current_layer_name");
+			parmasMap.put("layerName", layerName);
 		}
 		else{
 //			throw new IllegalArgumentException("null");
 			return false;
 		}
+		
 		
 		return false;
 	};
