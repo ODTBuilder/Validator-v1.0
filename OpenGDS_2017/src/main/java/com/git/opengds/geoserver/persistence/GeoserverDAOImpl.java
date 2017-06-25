335dc6b111167e9b37a058d2755ef089c3356ea7
SeulGiLee <ghre55@git.co.kr>
2017-06-26 00:16:59 +0900

    GeoserLayer 중복체크

---
+++ OpenGDS_2017/src/main/java/com/git/opengds/geoserver/persistence/GeoserverDAOImpl.java
@@ -0,0 +1,65 @@
+/*
+ *    OpenGDS/Builder
+ *    http://git.co.kr
+ *
+ *    (C) 2014-2017, GeoSpatial Information Technology(GIT)
+ *    
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 3 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package com.git.opengds.geoserver.persistence;
+
+import java.util.HashMap;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 파일에 대한 DB처리를 하는 클래스
+ * @author SG.Lee
+ * @Date 2017. 5. 12. 오전 2:24:03
+ * */
+@Repository
+public class GeoserverDAOImpl implements GeoserverDAO {
+	@Inject
+	private SqlSession sqlSession;
+
+	private static final String namespace = "com.git.mappers.geolayerMappers.GeolayerMapper";
+
+	/**
+	 * @since 2017. 6. 25.
+	 * @author SG.Lee
+	 * @param infoMap
+	 * @return
+	 * @see com.git.opengds.geoserver.persistence.GeoserverDAO#selectEditLayerDuplicateCheck(java.util.HashMap)
+	 */
+	@Override
+	public boolean selectEditLayerDuplicateCheck(HashMap<String,Object> infoMap){
+		int duplicateNums = 0;
+		try{
+			duplicateNums = sqlSession.selectOne(namespace + ".selectEditLayerDuplicateCheck", infoMap);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		boolean dupFlag = true;
+		
+		if(duplicateNums>0){
+			dupFlag = true;
+		}
+		else{
+			dupFlag = false;
+		}
+		return dupFlag;
+	}
+}
--- OpenGDS_2017/src/main/java/com/git/opengds/geoserver/service/GeoserverDBServiceImpl.java
+++ OpenGDS_2017/src/main/java/com/git/opengds/geoserver/service/GeoserverDBServiceImpl.java
@@ -3,33 +3,45 @@ package com.git.opengds.geoserver.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
 import com.git.gdsbuilder.FileRead.en.EnFileFormat;
 
-public class GeoserverDBServiceImpl {
+@Service
+public class GeoserverDBServiceImpl implements GeoserverDBService{
 
+	/*@Autowired
+	private Geoserver*/
+	
+	@Override
 	public boolean selectEditLayerDuplicateCheck(EnFileFormat fileFormat, String fileName, String layerName){
 		boolean dupFlag = true;
 		Map<String, Object> parmasMap = new HashMap<String, Object>();
 		if(fileFormat == EnFileFormat.DXF){
-			parmasMap.put("tableName", "public.qa10_layer_metadata");
+			parmasMap.put("fileCol", "collection_name");
+			parmasMap.put("colTableName", "public.qa10_layercollection");
+			parmasMap.put("metaTableName", "public.qa10_layer_metadata");
 			parmasMap.put("fileName", fileName);
 			parmasMap.put("nativeNameCol", "layer_id");
 			parmasMap.put("currentNameCol", "current_layer_name");
 			parmasMap.put("layerName", layerName);
 		}
 		else if(fileFormat == EnFileFormat.NGI){
-			parmasMap.put("tableName", "public.qa20_layer_metadata");
+			parmasMap.put("fileCol", "file_name");
+			parmasMap.put("colTableName", "public.qa20_layercollection");
+			parmasMap.put("metaTableName", "public.qa20_layer_metadata");
 			parmasMap.put("fileName", fileName);
 			parmasMap.put("nativeNameCol", "layer_name");
 			parmasMap.put("currentNameCol", "current_layer_name");
 			parmasMap.put("layerName", layerName);
 		}
 		else if(fileFormat == EnFileFormat.SHP){
+			/*parmasMap.put("tableName", "public.qa10_layer_metadata");
 			parmasMap.put("tableName", "public.qa10_layer_metadata");
 			parmasMap.put("fileName", fileName);
 			parmasMap.put("nativeNameCol", "layer_id");
 			parmasMap.put("currentNameCol", "current_layer_name");
-			parmasMap.put("layerName", layerName);
+			parmasMap.put("layerName", layerName);*/
 		}
 		else{
 //			throw new IllegalArgumentException("null");
